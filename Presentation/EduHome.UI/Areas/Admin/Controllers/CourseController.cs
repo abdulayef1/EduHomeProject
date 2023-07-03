2dea2821ed0ce248d3597654f8d6a2938618c920
abdulayef1 <7ka1cyr@code.edu.az>
2023-07-03 06:20:21 +0400

    admin panel done

---
+++ Presentation/EduHome.UI/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduHome.UI.Areas.Admin.Controllers;
+[Area("Admin")]
+public class CourseController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
